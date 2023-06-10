21ce8fa5fe621d696bf7d21d7374a0fb563afc28
forthelion <igorj1@live,com>
2023-06-09 23:35:43 -0700

    add imports to test

--- src/test/java/CILabTest.java
+++ src/test/java/CILabTest.java
@@ -1,6 +1,10 @@
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+Copy code
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
 
 import static org.junit.jupiter.api.Assertions.fail;
 
